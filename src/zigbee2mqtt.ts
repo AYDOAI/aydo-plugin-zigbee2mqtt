556d407e2ef86b555b27c3e9fdf4110f4dea40c3
Denis Aydo <pythonist001@proton.me>
2025-04-08 15:39:20 +0300

    Small fix ember check

--- src/zigbee2mqtt.ts
+++ src/zigbee2mqtt.ts
@@ -1787,8 +1787,12 @@ class Zigbee2mqtt extends baseDriverModule {
       this.log('Finded device', devicePort, device);
 
       const ember_substrings = ["10c4", "0457", "1a86"];
-      const check_ember_1 = ember_substrings.some(substring => device.vendorId.includes(substring));
-      const check_ember_2 = ["V2"].some(substring => device.pnpId.includes(substring));
+      const check_ember_1 = ember_substrings.some(
+        substring => typeof device.vendorId === 'string' && device.vendorId.includes(substring)
+      );
+      const check_ember_2 = ["V2"].some(
+        substring => typeof device.pnpId === 'string' && device.pnpId.includes(substring)
+      );
 
       if (this.logging) {
         this.log('getAdapterByPort', 'check_ember', check_ember_1, check_ember_2);
